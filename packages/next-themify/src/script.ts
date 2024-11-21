307c28e70835ad58051fec291dda6ae34504723f
Dario Corbinelli <corbinelli.dario.cd@gmail.com>
2024-11-21 18:38:27 +0100

    transitory commit

--- packages/next-themify/src/script.ts
+++ packages/next-themify/src/script.ts
@@ -1,5 +1,5 @@
 import { Prop } from './types/index'
-import { Available_Values, Default_Values, Fallback_Values, Handled_Props, Script_Params, Set_Storage_Values, Storage_Values, Storage_Values_Sanitization } from './types/script'
+import { Available_Values, Default_Values, Handled_Props, Script_Params, Set_SVs, Set_TAs, Storage_Values, SVs_Sanitization, TA_Sanitization } from './types/script'
 import { Nullable, UndefinedOr } from './types/utils'
 
 export function script({ config_SK, mode_SK, config, constants: { STRATS, MODES, COLOR_SCHEMES } }: Script_Params) {
@@ -8,7 +8,6 @@ export function script({ config_SK, mode_SK, config, constants: { STRATS, MODES,
   const handled_props = get_handled_props()
   const available_values = get_available_values()
   const default_values = get_default_values()
-  const fallback_values = get_fallback_values()
 
   // #region HELPERS --------------------------------------------------------------------------------
   function parse_JsonToMap(string: Nullable<string>): UndefinedOr<Map<string, string>> {
@@ -28,22 +27,6 @@ export function script({ config_SK, mode_SK, config, constants: { STRATS, MODES,
     } catch (error) {
       return undefined
     }
-  }
-
-  function is_SameMap(map1: Map<any, any>, map2: Map<any, any>) {
-    if (!(map1 instanceof Map) || !(map2 instanceof Map)) return false
-    if (map1.size !== map2.size) return false
-
-    for (const [key, value] of map1) {
-      if (!map2.has(key)) return false
-
-      const otherValue = map2.get(key)
-      if (value instanceof Map && otherValue instanceof Map) {
-        if (!is_SameMap(value, otherValue)) return false
-      } else if (value !== otherValue) return false
-    }
-
-    return true
   }
   // #endregion -------------------------------------------------------------------------------------
   // #region UTILS ----------------------------------------------------------------------------------
@@ -98,26 +81,6 @@ export function script({ config_SK, mode_SK, config, constants: { STRATS, MODES,
     return default_values.get(prop) === value
   }
   // #endregion -------------------------------------------------------------------------------------
-  // #region UTILS - fallback values ----------------------------------------------------------------
-  function get_fallback_values(): Fallback_Values {
-    const fallback_values: Map<Prop, string> = new Map()
-    for (const [key, value] of Object.entries(config)) {
-      const t_key = key as Prop
-
-      if (value.strategy === STRATS.mono) fallback_values.set(t_key, value.key)
-      else if (value.strategy === STRATS.custom || value.strategy === STRATS.multi) fallback_values.set(t_key, value.default)
-      else if (value.strategy === STRATS.light_dark) {
-        const fallback_value = value.enableSystem ? value.fallback : value.default
-        fallback_values.set(t_key, fallback_value)
-      }
-    }
-    return fallback_values
-  }
-  function is_fallback_value(prop: Prop, value: Nullable<string>) {
-    if (!value) return false
-    return fallback_values.get(prop) === value
-  }
-  // #endregion -------------------------------------------------------------------------------------
   // #endregion -------------------------------------------------------------------------------------
   // #region STORAGE VALUES (SVs) -------------------------------------------------------------------
   function merge_SVs(...values: Nullable<Storage_Values>[]): Storage_Values {
@@ -132,90 +95,217 @@ export function script({ config_SK, mode_SK, config, constants: { STRATS, MODES,
 
     return merged_values
   }
-  // #region STORAGE VALUES - sanitizer ------------------------------------------------------------
-  function sanitize_SVs(values: UndefinedOr<Map<string, string>>): Storage_Values_Sanitization {
-    let valid = true
-    const results: NonNullable<Storage_Values_Sanitization['results']> = new Map()
-    const sanitized_values: Storage_Values_Sanitization['sanitized_values'] = default_values
-
-    if (!values) return { valid: false, results, sanitized_values, fallback_values: default_values, available_values, handled_props, performed_on: values }
-
-    for (const [prop, value] of values) {
-      const handled_prop = prop as typeof handled_props extends Set<infer T> ? T : never
-      if (!handled_props.has(handled_prop)) {
-        valid = false
-        results.set(prop, { prop, is_handled: false, value, valid: false })
-        continue
-      }
-
-      if (!available_values.get(handled_prop)?.has(value)) {
-        valid = false
-        results.set(prop, { prop, is_handled: true, value, valid: false })
-        continue
-      }
-
-      results.set(prop, { prop, is_handled: true, value, valid: true })
-      sanitized_values.set(handled_prop, value)
-    }
-
-    for (const prop of handled_props) {
-      if (!values.has(prop)) {
-        valid = false
-        results.set(prop, { prop, is_handled: true, value: null, valid: false })
-      }
-    }
-
-    return { results, valid, sanitized_values, fallback_values: default_values, available_values, handled_props, performed_on: values }
-  }
-  // #endregion -------------------------------------------------------------------------------------
   // #region STORAGE VALUES - getter ----------------------------------------------------------------
   function get_SVs(): UndefinedOr<Map<string, string>> {
     const storage_string = localStorage.getItem(config_SK)
     return parse_JsonToMap(storage_string)
   }
   // #endregion -------------------------------------------------------------------------------------
+  // #region STORAGE VALUES - sanitizer ------------------------------------------------------------
+  function sanitize_SVs(prov_values: Nullable<Map<string, string>>): SVs_Sanitization {
+    const values: SVs_Sanitization['values'] = new Map()
+
+    for (const [prop, default_value] of default_values) {
+      const available_prop_values = available_values.get(prop) as NonNullable<ReturnType<(typeof available_values)['get']>>
+      const debug_props: NonNullable<ReturnType<SVs_Sanitization['values']['get']>> = {
+        prop,
+        was_provided: false,
+        is_handled: true,
+        value: undefined,
+        was_valid: false,
+        sanitized_value: default_value,
+        is_fallback: true,
+        available_values: available_prop_values,
+        default_value,
+      }
+      values.set(prop, debug_props)
+    }
+
+    if (!prov_values) return { handled_props, performed_on: prov_values, values, were_all_valid: false }
+
+    for (const [prop, value] of prov_values) {
+      if (!is_handled_prop(prop)) {
+        const debug_props: NonNullable<ReturnType<SVs_Sanitization['values']['get']>> = {
+          prop,
+          was_provided: true,
+          is_handled: false,
+          value,
+          was_valid: false,
+          sanitized_value: undefined,
+          is_fallback: undefined,
+          available_values: undefined,
+          default_value: undefined,
+        }
+        values.set(prop, debug_props)
+        continue
+      }
+
+      const handled_prop = prop as Prop
+      const available_prop_values = available_values.get(handled_prop) as NonNullable<ReturnType<(typeof available_values)['get']>>
+      const default_value = default_values.get(handled_prop) as NonNullable<ReturnType<(typeof default_values)['get']>>
+      if (!is_available_value(handled_prop, value)) {
+        const debug_props: NonNullable<ReturnType<SVs_Sanitization['values']['get']>> = {
+          prop,
+          was_provided: true,
+          is_handled: true,
+          value,
+          was_valid: false,
+          sanitized_value: default_value,
+          is_fallback: true,
+          available_values: available_prop_values,
+          default_value,
+        }
+        values.set(handled_prop, debug_props)
+        continue
+      }
+
+      const debug_props: NonNullable<ReturnType<SVs_Sanitization['values']['get']>> = {
+        prop,
+        was_provided: true,
+        is_handled: true,
+        value,
+        was_valid: true,
+        sanitized_value: value,
+        is_fallback: false,
+        available_values: available_prop_values,
+        default_value,
+      }
+      values.set(prop, debug_props)
+    }
+
+    const were_all_valid = Array.from(values.values()).every((i) => i.was_valid)
+    return { handled_props, performed_on: prov_values, values, were_all_valid }
+  }
+  // #endregion -------------------------------------------------------------------------------------
   // #region STORAGE VALUES - setter ----------------------------------------------------------------
-  function set_SVs(values: Storage_Values): Set_Storage_Values {
-    const info: Set_Storage_Values['info'] = new Map()
-    const storage_values: Storage_Values = new Map()
-    let must_update = false
+  function set_SVs(prov_values: Storage_Values): Set_SVs {
+    const values: Set_SVs['values'] = new Map()
 
-    const { results } = sanitize_SVs(get_SVs())
+    const curr_values = sanitize_SVs(get_SVs()).values
+    for (const [prop, value] of prov_values) {
+      
+    }
+  }
+  // function set_SVs(prov_values: Nullable<Storage_Values>): Set_SVs {
+  //   const values: Set_SVs['values'] = new Map()
+
+  //   const { values: curr_values } = sanitize_SVs(get_SVs())
+  //   if (!prov_values) {
+  //     for (const [prop, default_value] of default_values) {
+  //       const available_prop_values = available_values.get(prop) as NonNullable<ReturnType<(typeof available_values)['get']>>
+  //       const old_value = curr_values?.get(prop)?.value
+  //       const old_valid = curr_values?.get(prop)?.is_valid ?? false
+  //       const was_updated = old_value !== default_value
+  //       values.set(prop, {
+  //         prop,
+  //         was_provided: false,
+  //         is_handled: true,
+  //         old: { value: old_value, was_valid: old_valid },
+  //         new: { value: undefined, was_valid: false },
+  //         updated_value: default_value,
+  //         got_updated: true,
+  //         is_fallback: true,
+  //         available_values: available_prop_values,
+  //         default_value,
+  //       })
+  //     }
+  //     localStorage.setItem(config_SK, JSON.stringify(Object.fromEntries(default_values)))
+
+  //     return { handled_props, values }
+  //   }
+  // }
+  // console.log(set_SVs(undefined))
+  // function set_SVs(values: Storage_Values): Set_SVs {
+  //   const info: Set_SVs['info'] = new Map()
+  //   const storage_values: Storage_Values = new Map()
+  //   let must_update = false
+
+  //   const { results } = sanitize_SVs(get_SVs())
+  //   for (const prop of handled_props) {
+  //     const retrieved_value = results?.get(prop)?.value
+  //     const provided_value = values.get(prop) as NonNullable<ReturnType<(typeof values)['get']>>
+  //     const available_values = get_available_values().get(prop) as NonNullable<ReturnType<ReturnType<typeof get_available_values>['get']>>
+  //     const was_valid = results?.get(prop)?.valid ?? false
+  //     const is_same = retrieved_value === provided_value
+  //     const must_be_updated = !was_valid || (was_valid && !is_same)
+
+  //     if (must_be_updated) {
+  //       info.set(prop, { retrieved_value, provided_value, available_values, was_valid, is_same, updated: true })
+  //       storage_values.set(prop, provided_value)
+  //       must_update = true
+  //       continue
+  //     }
+
+  //     info.set(prop, { retrieved_value, provided_value, available_values, was_valid, is_same, updated: false })
+  //     storage_values.set(prop, retrieved_value as NonNullable<typeof retrieved_value>)
+  //   }
+
+  //   if (must_update) {
+  //     localStorage.setItem(config_SK, JSON.stringify(Object.fromEntries(storage_values)))
+  //     return { updated: true, updated_with: storage_values, info }
+  //   }
+  //   return { updated: false, sticked_with: values, info }
+  // }
+  // #endregion -------------------------------------------------------------------------------------
+  // #endregion -------------------------------------------------------------------------------------
+  // #region THEME ATTRIBUTES (TAs) -----------------------------------------------------------------
+  // #region THEME ATTRIBUTES - getter -------------------------------------------------------------
+  function get_TA(prop: Prop): Nullable<string> {
+    return html.getAttribute(`data-${prop}`)
+  }
+  // #endregion -------------------------------------------------------------------------------------
+  // #region THEME ATTRIBUTES - sanitizer ----------------------------------------------------------
+  function sanitize_TA({ prop, value }: { prop: Prop; value: Nullable<string> }): TA_Sanitization {
+    if (!handled_props.has(prop) || !value) return { results: { prop, is_handled: false, value, valid: false }, valid: false, fallback_value: undefined, default_value: undefined, available_values: undefined, handled_props }
+
+    const available_prop_values = available_values.get(prop) as NonNullable<ReturnType<ReturnType<typeof get_available_values>['get']>>
+    const default_value = default_values.get(prop) as NonNullable<ReturnType<(typeof default_values)['get']>>
+    const fallback_value = default_values.get(prop) as NonNullable<ReturnType<(typeof default_values)['get']>>
+
+    const valid = available_values.get(prop)?.has(value) ?? false
+    if (!valid) return { results: { prop, is_handled: true, value, valid: false }, valid: false, fallback_value, default_value, available_values: available_prop_values, handled_props }
+
+    return { results: { prop, is_handled: true, value, valid: true }, valid: true, sanitized_value: value, default_value, available_values: available_prop_values, handled_props }
+  }
+  // #endregion -------------------------------------------------------------------------------------
+  // #region THEME ATTRIBUTES - setter -------------------------------------------------------------
+  function set_TAs(values: Storage_Values): Set_TAs {
+    const info: Set_TAs = new Map()
+
     for (const prop of handled_props) {
-      const retrieved_value = results?.get(prop)?.value
+      const value = get_TA(prop)
+      const { results, available_values } = sanitize_TA({ prop, value }) as {
+        results: ReturnType<typeof sanitize_TA>['results']
+        available_values: NonNullable<ReturnType<typeof sanitize_TA>['available_values']>
+      }
+      const retrieved_value = results.value
       const provided_value = values.get(prop) as NonNullable<ReturnType<(typeof values)['get']>>
-      const available_values = get_available_values().get(prop) as NonNullable<ReturnType<ReturnType<typeof get_available_values>['get']>>
-      const was_valid = results?.get(prop)?.valid ?? false
+      const was_valid = results.valid
       const is_same = retrieved_value === provided_value
-      const must_be_updated = !was_valid || (was_valid && !is_same)
+      const must_update = !was_valid || (was_valid && !is_same)
 
-      if (must_be_updated) {
+      if (must_update) {
         info.set(prop, { retrieved_value, provided_value, available_values, was_valid, is_same, updated: true })
-        storage_values.set(prop, provided_value)
-        must_update = true
+        html.setAttribute(`data-${prop}`, provided_value)
         continue
       }
 
       info.set(prop, { retrieved_value, provided_value, available_values, was_valid, is_same, updated: false })
-      storage_values.set(prop, retrieved_value as NonNullable<typeof retrieved_value>)
     }
 
-    if (must_update) {
-      localStorage.setItem(config_SK, JSON.stringify(Object.fromEntries(storage_values)))
-      return { updated: true, updated_with: storage_values, info }
-    }
-    return { updated: false, sticked_with: values, info }
+    return info
   }
   // #endregion -------------------------------------------------------------------------------------
   // #endregion -------------------------------------------------------------------------------------
   // #region INITIALIZATION -------------------------------------------------------------------------
-  function init() {
-    const current_values = get_SVs()
-    const { sanitized_values } = sanitize_SVs(current_values)
-    set_SVs(sanitized_values)
-  }
+  // function init() {
+  //   const current_values = get_SVs()
+  //   const { results } = sanitize_SVs(current_values)
+  //   set_SVs(sanitized_values)
+  //   set_TAs(sanitized_values)
+  // }
 
   // #endregion -------------------------------------------------------------------------------------
 
-  init()
+  // init()
 }
--- packages/next-themify/src/types/script.ts
+++ packages/next-themify/src/types/script.ts
@@ -16,46 +16,63 @@ export type Script_Params = {
 export type Handled_Props = Set<Prop>
 export type Storage_Values = Map<Prop, string>
 export type Default_Values = Map<Prop, string>
-export type Fallback_Values = Map<Prop, string>
 export type Available_Values = Map<Prop, Set<string>>
 export type Mode_CSs = Map<string, CS>
 
-export type Storage_Values_Sanitization = {
-  results: UndefinedOr<Map<string, { prop: Nullable<string>; is_handled: boolean; value: Nullable<string>; valid: boolean }>>
-  valid: boolean
-  sanitized_values: Storage_Values
-  fallback_values: Fallback_Values
-  available_values: Available_Values
+export type SVs_Sanitization = {
   handled_props: Handled_Props
-  performed_on: Nullable<string> | Map<string, string>
-}
-export type Set_Storage_Values = {
-  info: Map<
-    Prop,
+  were_all_valid: boolean
+  performed_on: Nullable<Map<string, string>>
+  values: Map<
+    string,
     {
-      retrieved_value: Nullable<string>
-      provided_value: string
-      available_values: NonNullable<ReturnType<Available_Values['get']>>
+      prop: string
+      is_handled: boolean
+      value: Nullable<string>
       was_valid: boolean
-      is_same: boolean
-      updated: boolean
+      sanitized_value: UndefinedOr<string>
+      is_fallback: UndefinedOr<boolean>
+      was_provided: UndefinedOr<boolean>
+      available_values: ReturnType<Available_Values['get']>
+      default_value: ReturnType<Default_Values['get']>
     }
   >
-} & ({ updated: true; updated_with: Storage_Values } | { updated: false; sticked_with: Storage_Values })
-
-export type TA_Validation = {
-  results: { prop: Nullable<string>; is_handled: boolean; value: Nullable<string>; valid: boolean }
-  fallback_value: UndefinedOr<string>
-  valid: boolean
-  valid_TA: UndefinedOr<{ prop: Prop; value: string }>
-  available_values: UndefinedOr<Set<string>>
 }
+export type Set_SVs = {
+  handled_props: Handled_Props
+  values: Map<
+    string,
+    {
+      prop: string
+      is_handled: boolean
+      old: { value: Nullable<string>; was_valid: boolean }
+      new: { value: Nullable<string>; was_valid: boolean }
+      updated_value: string
+      was_provided: boolean
+      is_fallback: boolean
+      got_updated: boolean
+      available_values: ReturnType<Available_Values['get']>
+      default_value: ReturnType<Default_Values['get']>
+    }
+  >
+  performed_update: boolean
+}
+
+export type TA_Sanitization = {
+  results: { prop: Nullable<string>; is_handled: boolean; value: Nullable<string>; valid: boolean }
+  available_values: UndefinedOr<Set<string>>
+  default_value: UndefinedOr<string>
+  handled_props: Handled_Props
+} & ({ valid: true; sanitized_value: string } | { valid: false; fallback_value: UndefinedOr<string> })
 export type Set_TAs = Map<
   Prop,
   {
-    must_update: boolean
-    retrieved_TA: TA_Validation
-    received_TA: { prop: Prop; value: string }
+    retrieved_value: Nullable<string>
+    provided_value: string
+    available_values: NonNullable<ReturnType<Available_Values['get']>>
+    was_valid: boolean
+    is_same: boolean
+    updated: boolean
   }
 >
 
